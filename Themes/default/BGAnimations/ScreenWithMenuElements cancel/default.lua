7c1f96fa96999f3f1f3493e87728685c769024fe
Jonathan Payne <potentj@gmail.com>
2014-12-29 20:11:23 -0800

    Cancel

---
+++ Themes/default/BGAnimations/ScreenWithMenuElements cancel/default.lua
@@ -0,0 +1,43 @@
+local fTileSize = 32;
+local iTilesX = math.ceil( SCREEN_WIDTH/fTileSize );
+local iTilesY = math.ceil( SCREEN_HEIGHT/fTileSize );
+local fSleepTime = THEME:GetMetric( Var "LoadingScreen","ScreenOutDelay");
+--[[ local function Actor:PositionTile(self,iX,iY)
+	self:x( scale(iX,1,iTilesX,-SCREEN_CENTER_X,SCREEN_CENTER_X) );
+	self:y( scale(iY,1,iTilesY,-SCREEN_CENTER_Y,SCREEN_CENTER_Y) );
+end --]]
+local t = Def.ActorFrame {
+	InitCommand=cmd();
+	OnCommand=cmd(sleep,fSleepTime);
+	Def.Quad {
+		InitCommand=cmd(Center;zoomto,SCREEN_WIDTH,SCREEN_HEIGHT);
+		StartTransitioningCommand=cmd(diffuse,Color.Black;diffusealpha,0;decelerate,0.125;diffusealpha,1);
+	}
+};
+--[[ for indx=1,iTilesX do
+	for indy=1,iTilesY do
+		t[#t+1] = Def.Quad {
+			InitCommand=cmd(zoom,fTileSize-2;
+				x,math.floor( scale(indx,1,iTilesX,(iTilesX/2)*fTileSize*-1,(iTilesX/2)*fTileSize*1) );
+				y,math.floor( scale(indy,1,iTilesY,(iTilesY/2)*fTileSize*-1,(iTilesY/2)*fTileSize*1) );
+			);
+			OnCommand=cmd(diffuse,Color("Black");diffusealpha,0;zoom,fTileSize*1.25;linear,0.0325 + ( indx / 60 );diffusealpha,1;zoom,fTileSize-2);
+		};
+	end
+end --]]
+t[#t+1] = Def.ActorFrame {
+	InitCommand=cmd(x,SCREEN_WIDTH-32;y,SCREEN_HEIGHT-32;pulse;effectmagnitude,1,1.25,0;effectclock,'timerglobal';effectperiod,0.45),
+	OnCommand=cmd(sleep,fSleepTime),
+	--
+	LoadActor(THEME:GetPathG("Player","Badge FullCombo")) .. {
+		StartTransitioningCommand=cmd(diffuse,Color.Yellow;zoom,0;diffusealpha,0;linear,0.1;zoom,1;diffusealpha,1)
+	}
+};
+--[[ return Def.ActorFrame {
+	Def.Quad {
+		InitCommand=cmd(Center;zoomto,SCREEN_WIDTH+1,SCREEN_HEIGHT);
+		OnCommand=cmd(diffuse,color("0,0,0,1");linear,0.15;diffusealpha,0);
+	};
+}; --]]
+
+return t
